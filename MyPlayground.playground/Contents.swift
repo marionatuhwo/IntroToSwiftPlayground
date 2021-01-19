c4677927bdac41b0698d26e64b704c23e66ee659
Marion <ano@hawaii.edu>
2021-01-19 10:27:50 -1000

    SP#1 Part 1 - Part 7 complete
    Class 3 Tuesday January 19

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,33 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//declare a variable, name it, myfirstVariable and type it as a String
+var myFirstVariable:String
+//String is a data type that is represented by a series of characters. The string must be surrounded by ""
+
+//assign myFirstVariable to a string
+myFirstVariable = "Iʻve created my first string"
+
+//String Concatenation - combining strings together
+
+//declare and assign a second variable all in one line
+var mySecondVariable = "and look I combine 2 strings together"
+
+//create a third variable to hold the string concatenation
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+print(myThirdVariable)
+
+
+
+
+
+
+
+
+
+
+
+
