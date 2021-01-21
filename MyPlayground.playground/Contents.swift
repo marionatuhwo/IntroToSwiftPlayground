022113d2bcbeb6e949370e95677fcac12fd73044
Marion <ano@hawaii.edu>
2021-01-21 10:46:41 -1000

    Assignment #2 Completed parts 1-6
    Class 4 January 21, 2021

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -20,6 +20,25 @@ var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
 
+//Part 8 - SW#1
+
+
+
+//Assignment #2: Introduction to True and False Class 4 Thursday 1.21.21
+
+var appState = false
+
+//print() - print function - it prints into the console- all functions have a name and followed by parenthesis ()
+print(appState)
+
+//declared and assigned two variables of type int
+var numberOne = 5
+var numberTwo = 7
+
+var comparison = numberOne > numberTwo
+
+print(comparison)
+
 
 
 
